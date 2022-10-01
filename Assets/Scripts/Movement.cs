464b64b3f59f264e4b20077e6b3e845957327451
richardsha19 <113735719+richardsha19@users.noreply.github.com>
2022-10-01 15:39:24 -0400

    Movement Without Jumping Fixeds

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+
+
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Threading;
@@ -6,76 +8,53 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private float playerSpeed = 2.0f;
-    private float jumpHeight = 1.0f;
     public CharacterController controller;
-    private float gravityValue = -9.81f;
-    private bool groundedPlayer;
-    //private PlayerMotor motor;
-    private bool grounded = false;
-    Rigidbody rb;
-    private bool jump_pressed = false;
+    public Transform cam;
 
-    public float turnSmoothTime = 0.1f;
+    public float speed = 6;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3;
+    Vector3 velocity;
+    bool isGrounded;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
     float turnSmoothVelocity;
+    public float turnSmoothTime = 0.1f;
 
-    public Transform camera;
-    private Vector3 PlayerVelocity;
-
-
-    Vector3 MovementJump()
-    {
-        groundedPlayer = controller.isGrounded;
-
-        Debug.Log(groundedPlayer);
-
-        if (groundedPlayer)
-        {
-            PlayerVelocity.y = 0.0f;
-        }
-
-        if(Input.GetButtonDown("Jump") && groundedPlayer)
-        {
-            PlayerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-            jump_pressed = false;
-        }
-
-        PlayerVelocity.y += gravityValue * Time.deltaTime;
-        //controller.Move(PlayerVelocity.normalized * Time.deltaTime);
-
-        return PlayerVelocity;
-
-        
-    }
-
+    // Update is called once per frame
     void Update()
     {
-        groundedPlayer = controller.isGrounded;
-        
-        Debug.Log(controller.isGrounded);
+        //jump
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
 
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+        //gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+        //walk
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-
-        Vector3 direction = new Vector3(horizontal, 0.0f, vertical).normalized;// normalize: Can't go faster if horizontal and vertical keys are pressed
-
-        if (controller.velocity.y == 0)
-        {
-            direction += MovementJump();
-        }
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + camera.eulerAngles.y; //arctan
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * playerSpeed * Time.deltaTime);
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
     }
-
-    // Update is called once per frame
 }
