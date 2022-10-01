ecdd1c0957a92d02ef3b4598f3983df0eeb564b3
richardsha19 <113735719+richardsha19@users.noreply.github.com>
2022-10-01 15:08:47 -0400

    Updated Code

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,50 +7,61 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
-    private CharacterController controller;
-    private Vector3 PlayerVelocity;
     private float playerSpeed = 2.0f;
     private float jumpHeight = 1.0f;
     public CharacterController controller;
-    public float playerSpeed = 2.0f;
-    private float jumpHeight = 30.0f;
     private float gravityValue = -9.81f;
     private bool groundedPlayer;
-    Rigidbody rb =
     //private PlayerMotor motor;
     private bool grounded = false;
     Rigidbody rb;
-    private bool isJumping = false;
+    private bool jump_pressed = false;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
     public Transform camera;
+    private Vector3 PlayerVelocity;
 
 
-    void Start()
+    void MovementJump()
     {
+        groundedPlayer = controller.isGrounded;
 
-        //rb = GetComponent<Rigidbody>();
-        //motor = GetComponent<PlayerMotor>();
-        //controller = gameObject.AddComponent<CharacterController>();
+        if (groundedPLayer)
+        {
+            PlayerVelocity.y = 0.0f;
+        }
+
+        if(jump_pressed && groundedPlayer)
+        {
+            PlayerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            jump_pressed = false;
+        }
+
+        PlayerVelocity.y += gravityValue * Time.deltaTime;
+        controller.move(PlayerVelocity * time);
+
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        groundedPlayer = controller.isGrounded;
 
-            float horizontal = Input.GetAxisRaw("Horizontal");
+
+        if(controller.velocity.y == 0)
+        {
+            
+        }
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        float jump = 0f;
 
-        Vector3 direction = new Vector3(horizontal, jump, vertical).normalized;// normalize: Can't go faster if horizontal and vertical keys are pressed
-        //Debug.Log(controller.isGrounded);
+        Vector3 direction = new Vector3(horizontal, 0.0f, vertical).normalized;// normalize: Can't go faster if horizontal and vertical keys are pressed
 
         if (direction.magnitude >= 0.1f)
         {
-
             float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + camera.eulerAngles.y; //arctan
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
